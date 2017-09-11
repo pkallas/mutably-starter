2aa28c84944d0cbb955b8535d0ce15b135a7a64f
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-09-11 15:37:30 -0700

    Added functionality to get one pokemon

--- public/script.js
+++ public/script.js
@@ -14,12 +14,12 @@ $(document).ready(function () {
     if (htmlElements.pokemonTable.find('td').length > 0) {
       return;
     };
-    // If not, fetch the data and add it to the table
+    // If not, fetch the pokemon data and add it to the table
     fetch('http://mutably.herokuapp.com/pokemon')
     .then(pokemonData => pokemonData.json())
-    .then(formattedPokemonData => {
-      for (var index in formattedPokemonData) {
-        let allPokemonData = formattedPokemonData[index];
+    .then(parsedPokemonData => {
+      for (var index in parsedPokemonData) {
+        let allPokemonData = parsedPokemonData[index];
         allPokemonData.forEach(index => {
           htmlElements.pokemonTable.append(`<tr id=${index.name}>
             <td>${index.name}</td>
@@ -32,4 +32,35 @@ $(document).ready(function () {
     })
     .catch(error => console.log('Oh no: ' + error));
   });
+
+  // Add a click event listener for searchForPokemonSubmit button
+  htmlElements.seeOnePokemon.click(function () {
+    // First, prevent the form from posting data
+    event.preventDefault();
+    // Then, empty the table if there is data
+    clearTable();
+    // Next, obtain the form data
+    let formData = $('form').serializeArray();
+    // Next, fetch the pokemon data, and add the data that matches the submitted name to the table
+    fetch('http://mutably.herokuapp.com/pokemon')
+    .then(pokemonData => pokemonData.json())
+    .then(parsedPokemonData => {
+      for (var index in parsedPokemonData) {
+        let allPokemonData = parsedPokemonData[index];
+        let filteredPokemonData = allPokemonData.filter(index => index.name === formData[0].value);
+        htmlElements.pokemonTable.append(`<tr id=${filteredPokemonData[0].name}>
+          <td>${filteredPokemonData[0].name}</td>
+          <td>${filteredPokemonData[0].pokedex}</td>
+          <td>${filteredPokemonData[0].evolves_from}</td>
+          <td>${filteredPokemonData[0].image}</td>
+        </tr>`);
+      }
+    })
+    .catch(error => console.log('Oh no: ' + error));
+  });
+
+  // Create a function that clears the table data
+  function clearTable() {
+    htmlElements.pokemonTable.empty();
+  };
 });
